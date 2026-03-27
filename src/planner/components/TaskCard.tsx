c3bdb1004a1eccaefee46a9aa7991fe7b498c21f
Gerald López G <64161345+gerald-lg@users.noreply.github.com>
2026-03-26 21:45:14 -0300

    refactor TaskCard component to include note and state props

--- src/planner/components/TaskCard.tsx
+++ src/planner/components/TaskCard.tsx
@@ -1,18 +1,28 @@
-import type { StateTask } from "../models"
+import { stateTaskLabels } from "../models";
 
 interface TaskCardProps {
     title: string;
     duration?: number;
     note?: string;
-    state?: StateTask;
+    state?: keyof typeof stateTaskLabels;
+
 }
 
-export const TaskCard = ({ title, duration }: TaskCardProps) => {
+export const TaskCard = ({ title, duration, note, state }: TaskCardProps) => {
   return (
     <section>
         <div className="text-white">
-          <h4 className="font-bold">{title}</h4>
-          {duration && <p className="text-sm text-gray-500">Duration: {duration} mins</p>}
+          <input
+            type="text"
+            placeholder="Enter title..."
+            className="text-xl font-bold text-white w-full placeholder:text-gray-100 focus:outline-none bg-transparent text-ellipsis whitespace-nowrap overflow-hidden"
+            value={title}
+          />
+          { note && <p className="text-xs text-white text-ellipsis whitespace-nowrap overflow-hidden">{note}</p>}
+          <section className="flex flex-row items-center justify-end gap-2">
+            { state && <p className={`badge ${state}`}>{stateTaskLabels[state]}</p>}
+            {duration && <p className="text-xs text-white">({duration} mins)</p>}
+          </section>
         </div>
     </section>
   )
