1164dab95c569773efc8f8d52eef9ed99ec8b9f1
Gerald López G <64161345+gerald-lg@users.noreply.github.com>
2026-03-28 18:06:54 -0300

    feat(templateTask): add initial reducer for task draft management

---
+++ src/planner/reducers/templateTask.reducer.ts
@@ -0,0 +1,15 @@
+
+export const templateTaskReducer = (state = {}, action ) => {
+
+    switch(action.type){
+
+        case 'ADD_TASK_DRAFT':
+            break;
+        case 'CHANGE_TASK_TITLE':
+            break;
+        case 'SAVE_TASK':
+            break;
+        case 'DISCARD_TASK':
+            break;
+    }
+}
