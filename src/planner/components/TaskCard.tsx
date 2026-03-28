d9263f3fd9709bb7862556bde2f7444672651c4f
Gerald López G <64161345+gerald-lg@users.noreply.github.com>
2026-03-27 22:33:07 -0300

    refactor(TaskCard): update props to include onBlur and ref, and adjust onSubmit handling

--- src/planner/components/TaskCard.tsx
+++ src/planner/components/TaskCard.tsx
@@ -1,3 +1,4 @@
+import type { Ref, SubmitEvent } from "react";
 import { stateTaskLabels } from "../models";
 
 interface TaskCardProps {
@@ -7,20 +8,34 @@ interface TaskCardProps {
     note?: string;
     state?: keyof typeof stateTaskLabels;
     onChange: (value: string, id: string) => void;
-    onSubmit: () => void;
+    onSubmit: (e: SubmitEvent<HTMLFormElement>, id: string) => void;
+    onBlur: (value: string, id: string) => void;
+    ref: Ref<HTMLInputElement>
 }
 
-export const TaskCard = ({ id, title, duration, note, state, onChange, onSubmit }: TaskCardProps) => {
+export const TaskCard = (
+    { id, 
+      title, 
+      duration, 
+      note, 
+      state, 
+      onChange, 
+      onSubmit,
+      onBlur,
+      ref
+    }: TaskCardProps) => {
   return (
-    <form onSubmit={onSubmit}>
+    <form onSubmit={(e) => onSubmit(e, id)}>
         <div className="text-white">
           <input
+            ref={ref}
             name="title"
             type="text"
             placeholder="Enter title..."
             className="text-md font-semibold text-white w-full placeholder:text-gray-100 focus:outline-none bg-transparent text-ellipsis whitespace-nowrap overflow-hidden"
             value={title}
             onChange={(e) => onChange(e.target.value, id)}
+            onBlur={(e) => onBlur(e.target.value, id)}
           />
           { note && <p className="text-xs text-white text-ellipsis whitespace-nowrap overflow-hidden">{note}</p>}
           <section className="flex flex-row items-center justify-end gap-2">
