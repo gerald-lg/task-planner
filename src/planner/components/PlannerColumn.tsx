fae061b32a2048fc3b2cbd566db096720172ebf4
Gerald López G <64161345+gerald-lg@users.noreply.github.com>
2026-03-29 17:12:29 -0300

    feat(PlannerColumn): create PlannerColumn component with conditional button rendering

---
+++ src/planner/components/PlannerColumn.tsx
@@ -0,0 +1,49 @@
+import { type ReactNode } from "react";
+
+import type { ColorType } from "../models";
+import { colorClasses } from "../helpers";
+
+type PlannerColumnBaseProps = {
+  id: string;
+  children?: ReactNode;
+  counter: number;
+  name: string;
+  color?: ColorType;
+};
+
+type PlannerColumnWithButton = PlannerColumnBaseProps & {
+  showAddButton: true;
+  handleAddTask: () => void;
+  handleMouseDown: (e: React.MouseEvent<HTMLButtonElement>) => void;
+};
+
+type PlannerColumnWithoutButton = PlannerColumnBaseProps & {
+  showAddButton?: false;
+  handleAddTask?: never;
+  handleMouseDown?: never;
+};
+
+type PlannerColumnProps = PlannerColumnWithButton | PlannerColumnWithoutButton;
+
+export const PlannerColumn = ({ children, counter, name, color, showAddButton, handleAddTask, handleMouseDown }: PlannerColumnProps) => {
+    const sectionColorClass = color ? colorClasses[color].section : "";
+    const badgeColorClass = color ? colorClasses[color].badge : "";
+
+    return (
+        <section className={`flex flex-col gap-2 p-4 w-2/5 rounded-lg ${sectionColorClass}`}>
+            <div className="flex flex-row justify-between">
+                <div className="flex flex-row gap-2 items-center">
+                    <h3 className={`p-1 rounded ${badgeColorClass} text-white`}>{ name }</h3>
+                    <span className="text-sm text-white">{ counter }</span>
+                </div>
+                {  
+                    showAddButton 
+                    && <button onMouseDown={(e) => handleMouseDown(e)} onClick={handleAddTask} className="px-2 py-1 text-white">+</button> 
+                }
+            </div>
+            <section className={`flex flex-col gap-3 p-2` }>
+                {children}
+            </section>
+        </section>
+    )
+}
--- src/planner/components/index.ts
+++ src/planner/components/index.ts
@@ -1,2 +1,2 @@
-export * from "./ListTask";
+export * from "./PlannerColumn";
 export * from "./TaskCard";
