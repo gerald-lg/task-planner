cc7459de0676cb4c00d230806385a00c94895290
Gerald López G <64161345+gerald-lg@users.noreply.github.com>
2026-03-29 21:00:32 -0300

    feat(useTemplateTask): implement useTemplateTask hook for task management

---
+++ src/planner/hooks/useTemplateTask.ts
@@ -0,0 +1,56 @@
+import { useReducer, useState, type SubmitEvent } from "react";
+import type { TaskTemplate } from "../models";
+import { templateTaskReducer } from "../reducers";
+
+
+export const useTemplateTask = (initialState: TaskTemplate[]) => {
+    const [tasks, dispatch] = useReducer(templateTaskReducer, initialState);
+    const [pendingFocusID, setPendingFocusID] = useState<string|null>(null);
+      
+
+    const handleAddTask = () => {
+        const id = crypto.randomUUID();
+        dispatch({ type: 'ADD_TASK_DRAFT', payload: { id } });
+        setPendingFocusID(id);
+    }
+
+    const focusTaskDraft = (e : React.MouseEvent<HTMLButtonElement>) => {
+        e.preventDefault();
+
+        const draftTask = tasks.find((task) => task.isDraft);
+        if(draftTask){
+            setPendingFocusID(draftTask.id);
+        }
+    }
+
+    const handleChangeTask = (value: string, id: string) => {
+        dispatch({ type: 'CHANGE_TASK_TITLE', payload: { id, title: value } }); 
+    }
+
+    const handleOnBlurTask = (value: string, id: string) => {
+        if(value.trim() === ""){
+            dispatch({ type: 'DISCARD_TASK', payload: { id } });
+        }
+        else{
+            dispatch({ type: 'SAVE_TASK', payload: { id } });
+        }
+    }
+
+    const handleSubmitTask = (e:SubmitEvent<HTMLFormElement>, id:string) => {
+        e.preventDefault();
+        dispatch({ type: 'SAVE_TASK', payload: { id } });
+    }
+
+    return {
+        // variables
+        tasks,
+        pendingFocusID,
+        // handlers
+        handleAddTask,
+        focusTaskDraft,
+        handleChangeTask,
+        handleOnBlurTask,
+        handleSubmitTask,
+        setPendingFocusID  
+    }
+}
