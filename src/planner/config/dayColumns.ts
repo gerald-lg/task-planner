2111e8113024c8d80a4067c92efb5e9404f47779
Gerald López G <64161345+gerald-lg@users.noreply.github.com>
2026-04-08 22:42:20 -0400

    feat(dayColumns): add dayColumns configuration for weekly planner

---
+++ src/planner/config/dayColumns.ts
@@ -0,0 +1,45 @@
+import type { ColorType, Day } from "../models";
+
+interface DayColumn {
+  id: Day;
+  name: string;
+  color: ColorType;
+}
+
+export const dayColumns: DayColumn[] = [
+  {
+    id: "monday",
+    name: "Monday",
+    color: "emerald",
+  },
+  {
+    id: "tuesday",
+    name: "Tuesday",
+    color: "blue",
+  },
+  {
+    id: "wednesday",
+    name: "Wednesday",
+    color: "rose",
+  },
+  {
+    id: "thursday",
+    name: "Thursday",
+    color: "amber",
+  },
+  {
+    id: "friday",
+    name: "Friday",
+    color: "indigo",
+  },
+  {
+    id: "saturday",
+    name: "Saturday",
+    color: "cyan",
+  },
+  {
+    id: "sunday",
+    name: "Sunday",
+    color: "orange",
+  },
+];
