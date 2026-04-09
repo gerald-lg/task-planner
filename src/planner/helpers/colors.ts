6586ec7fe01821bb1912037397d801cb5b1272a2
Gerald López G <64161345+gerald-lg@users.noreply.github.com>
2026-04-08 22:42:25 -0400

    feat(colors): expand colorClasses with additional color configurations

--- src/planner/helpers/colors.ts
+++ src/planner/helpers/colors.ts
@@ -1,18 +1,53 @@
 export const colorClasses = {
+    amber: {
+        section: "bg-amber-950",
+        badge: "bg-amber-700",
+        card: "bg-amber-700",
+    },
     blue: {
         section: "bg-blue-950",
         badge: "bg-blue-700",
         card: "bg-blue-700",
+    },
+    cyan: {
+        section: "bg-cyan-950",
+        badge: "bg-cyan-700",
+        card: "bg-cyan-700",
+    },
+    emerald: {
+        section: "bg-emerald-950",
+        badge: "bg-emerald-700",
+        card: "bg-emerald-700",
     },
     green: {
         section: "bg-green-950",
         badge: "bg-green-700",
         card: "bg-green-700",
     },
+    indigo: {
+        section: "bg-indigo-950",
+        badge: "bg-indigo-700",
+        card: "bg-indigo-700",
+    },
+    lime: {
+        section: "bg-lime-950",
+        badge: "bg-lime-700",
+        card: "bg-lime-700",
+    },
+    orange: {
+        section: "bg-orange-950",
+        badge: "bg-orange-700",
+        card: "bg-orange-700",
+    },
     red: {
         section: "bg-red-950",
         badge: "bg-red-700",
         card: "bg-red-700",
+    },
+    rose: {
+        section: "bg-rose-950",
+        badge: "bg-rose-700",
+        card: "bg-rose-700",
     },
     yellow: {
         section: "bg-yellow-950",
