56661efb1ea95bb4654d3fca1148e305c0f8589a
Gerald López G <64161345+gerald-lg@users.noreply.github.com>
2026-04-11 21:47:54 -0400

    feat(modal): create index file to export modal components and types

---
+++ src/planner/components/shared/modal/index.ts
@@ -0,0 +1,9 @@
+export * from "./parts/ModalContent";
+export * from "./parts/ModalContext";
+export * from "./parts/ModalRoot";
+export * from "./parts/ModalTrigger";
+export * from "./types";
+export * from "./hooks";
+
+export * from "./Modal";
+export * from "./variants/EditTaskModalContent";
